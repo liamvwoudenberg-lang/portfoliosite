c20734ede9bc78b12cbc696b066d5bb090fb3867
liamvwoudenberg-lang <liamvwoudenberg@gmail.com>
2026-01-14 19:58:23 +0100

    Refactor Vite config to use environment variables
    Updated Vite configuration to load environment variables and changed the base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/portfoliosite/',
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: '/portfolio/',
+    plugins: [react()],
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+  };
+});
