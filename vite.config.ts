073aa092a467ae0516d43ad9894cb07b2d8e3151
liamvwoudenberg-lang <liamvwoudenberg@gmail.com>
2026-01-14 20:03:45 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/portfolio/',
+    base: '/portfolio/', // This must match your GitHub repository name
     plugins: [react()],
     server: {
       port: 3000,
