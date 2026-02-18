906d9fc82e9ad6c7f30309a32e482839edf3beca
liamvwoudenberg-lang <liamvwoudenberg@gmail.com>
2026-02-18 12:00:16 +0100

    Update comment in vite.config.ts
    Added a comment about loading environment variables.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Load env vars from the current directory
+  // Load env vars from the current directory poop
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
